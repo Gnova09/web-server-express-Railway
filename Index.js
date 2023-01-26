fb82267ae841ee0e20fe4365652a581d32903927
Georges Bueno <georges.bueno@hotmail.com>
2023-01-26 13:13:36 -0400

    url modify

--- Index.js
+++ Index.js
@@ -22,5 +22,5 @@ app.get('*', (req, res)=> {
  */
 
 app.listen(port,()=>{
-    console.log(`Listening at http://localhost:${port}`)
+    console.log(`Listening at http://web-server-express-railway-production.up.railway.app:${port}`)
 })
